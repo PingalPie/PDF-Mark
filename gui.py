b34435967567a7e664313a3c687f883abc4658c4
PingalPie <lscience611@gmail.com>
2023-02-25 07:47:23 +0530

    fixed open_file issue and added comments in gui.py

--- gui.py
+++ gui.py
@@ -27,7 +27,7 @@ class window(tk.Tk):
             return
 
         for function in dir(functions):
-            if not function.startswith('__') and not function=='select_file':
+            if not function.startswith('__') and not function=='select_file_pdf' and not function=='select_file_image' and not function=='select_output_file_pdf':
                 options.append(function)
 
         option_menu = tk.OptionMenu(self.frame1, option, *options)
@@ -46,10 +46,12 @@ class window(tk.Tk):
         self.frame1.pack_forget()
         match selected_option:
             case 'split_pdf':
+                # Creating required labels and frames
                 title = tk.Label(self, text='Split Pdf', font='monospace 20')
                 title.pack()
                 frame = tk.Frame(self)
                 
+                # Creating variables for entryboxes
                 input_path = tk.StringVar()
                 input_path.set('')
                 output_path = tk.StringVar()
@@ -59,6 +61,7 @@ class window(tk.Tk):
                 end_page = tk.StringVar()
                 end_page.set('')
 
+                # Actual function which will do the task
                 def split_pdfs(input_path, output_path, start_page, end_page):                    
                     result = self.funcs.split_pdf(str(input_path), str(output_path), int(start_page), int(end_page))
                     frame.pack_forget()
@@ -69,23 +72,26 @@ class window(tk.Tk):
                     back_to_home.pack()
                     frame2.pack()
 
-                submit_button = tk.Button(frame, text='Submit', command=lambda: split_pdfs(input_path=input_path.get(), output_path=output_path.get(), start_page=start_page.get(), end_page=end_page.get()))
-
+                # Creating entry boxes
                 input_path_box = tk.Entry(frame, textvariable=input_path)
                 output_path_box = tk.Entry(frame, textvariable=output_path)
                 start_page_box = tk.Entry(frame, textvariable=start_page)
                 end_page_box = tk.Entry(frame, textvariable=end_page)
 
+                # Creating Labels for entry boxes
                 input_path_label = tk.Label(frame, text='Input Path: ')
                 output_path_label = tk.Label(frame, text='Output Path: ')
                 start_page_label = tk.Label(frame, text='Start Page: ')
                 end_page_label = tk.Label(frame, text='End Page: ')
 
-                input_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (input_path.set(self.funcs.select_file()), input_path_box.update()))
-                output_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (output_path.set(self.funcs.select_file()), output_path_box.update()))
+                # Creating required buttons
+                input_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (input_path.set(self.funcs.select_file_pdf()), input_path_box.update()))
+                output_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (output_path.set(self.funcs.select_output_file_pdf()), output_path_box.update()))
+                submit_button = tk.Button(frame, text='Submit', command=lambda: split_pdfs(input_path=input_path.get(), output_path=output_path.get(), start_page=start_page.get(), end_page=end_page.get()))
 
                 back_to_homepage = tk.Button(frame, text='Back to HomePage', command=lambda: (title.pack_forget(), frame.pack_forget(), self.window()))
 
+                # Packing everything
                 input_path_label.grid(column=1, row=1)
                 output_path_label.grid(column=1, row=2)
                 start_page_label.grid(column=1, row=3)
@@ -106,15 +112,18 @@ class window(tk.Tk):
                 frame.pack()
             
             case 'reduce_pdf_size':
+                # Creating required labels and frames
                 title = tk.Label(self, text="Reduce file size of a pdf", font='Sans-Serif 20')
                 title.pack()
                 frame = tk.Frame(self)
 
+                # Creating variables for entryboxes
                 input_path = tk.StringVar()
                 input_path.set('')
                 output_path = tk.StringVar()
                 output_path.set('')
 
+                # Actual function which will do the task
                 def reduce_file_size(file, out_file):
                     result = self.funcs.reduce_pdf_size(file, out_file)
                     frame.pack_forget()
@@ -125,18 +134,21 @@ class window(tk.Tk):
                     back_to_home.pack()
                     frame2.pack()
 
+                # Creating entry boxes
                 input_path_box = tk.Entry(frame, textvariable=input_path)
                 output_path_box = tk.Entry(frame, textvariable=output_path)
 
+                # Creating Labels for entry boxes
                 input_path_label = tk.Label(frame, text='Input Path: ')
                 output_path_label = tk.Label(frame, text='Output Path: ')
 
-                input_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (input_path.set(self.funcs.select_file()), input_path_box.update()))
-                output_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (output_path.set(self.funcs.select_file()), output_path_box.update()))
-
+                # Creating required buttons
+                input_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (input_path.set(self.funcs.select_file_pdf()), input_path_box.update()))
+                output_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (output_path.set(self.funcs.select_output_file_pdf()), output_path_box.update()))
                 submit_button = tk.Button(frame, text='Submit', command=lambda: reduce_file_size(input_path.get(), output_path.get()))
                 back_to_homepage = tk.Button(frame, text='Back to HomePage', command=lambda: (title.pack_forget(), frame.pack_forget(), self.window()))
 
+                # Packing everything
                 input_path_label.grid(column=1, row=1)
                 output_path_label.grid(column=1, row=2)
 
@@ -153,59 +165,130 @@ class window(tk.Tk):
                 frame.pack()
             
             case 'merge_pdfs':
+                # Creating required labels and frames
                 title = tk.Label(self, text="Merge Pdfs", font='Sans-Serif 20')
-                title.pack()
+                instructions = tk.Label(self, text='Have values in the entry box of list of files comma seperated', relief=tk.SUNKEN, anchor='s')
                 frame = tk.Frame(self)
 
-                file1 = tk.StringVar()
-                file1.set('')
-                file2 = tk.StringVar()
-                file2.set('')
+                # Creating variables for entryboxes
+                files = tk.StringVar()
+                files.set('')
                 output_path = tk.StringVar()
                 output_path.set('')
 
-                def merge_pdfs(list_of_files, output_file):
+                # Actual function which will do the task
+                def merge_pdfs(output_file):
+                    list_of_files = files.get().split(',')
                     result = self.funcs.merge_pdfs(list_of_files, output_file)
                     frame.pack_forget()
                     frame2 = tk.Frame(self)
                     label12 = tk.Label(frame2, text=result, font='sans-serif 15')
                     label12.pack()
-                    back_to_home = tk.Button(frame2, text='Back to HomePage', command=lambda: (title.pack_forget(), frame2.pack_forget(), self.window()))
+                    back_to_home = tk.Button(frame2, text='Back to HomePage', command=lambda: (instructions.pack_forget(), title.pack_forget(), frame2.pack_forget(), self.window()))
                     back_to_home.pack()
                     frame2.pack()
 
-                file1_box = tk.Entry(frame, textvariable=file1)
-                file2_box = tk.Entry(frame, textvariable=file2)
+                # Creating entry boxes
+                files_box = tk.Entry(frame, textvariable=files)
                 output_path_box = tk.Entry(frame, textvariable=output_path)
 
-                file1_label = tk.Label(frame, text='File 1: ')
-                file2_label = tk.Label(frame, text='File 2: ')
+                # Creating Labels for entry boxes
+                files_label = tk.Label(frame, text='Files: ')
                 output_path_label = tk.Label(frame, text='Output Path: ')
 
-                submit_button = tk.Button(frame, text='Submit', command=lambda: (frame.pack_forget(), merge_pdfs([file1.get(), file2.get()], output_path.get())))
-                back_to_homepage = tk.Button(frame, text='Back to HomePage', command=lambda: (title.pack_forget(), frame.pack_forget(), self.window()))
+                # Creating required buttons
+                submit_button = tk.Button(frame, text='Submit', command=lambda: (merge_pdfs(output_path.get())))
+                back_to_homepage = tk.Button(frame, text='Back to HomePage', command=lambda: (instructions.pack_forget(), title.pack_forget(), frame.pack_forget(), self.window()))
+                if files.get() == '':
+                    files_open_file = tk.Button(frame, text="Open File", command=lambda: (files.set(self.funcs.select_file_pdf()),files_box.update()))
+                files_open_file = tk.Button(frame, text="Open File", command=lambda: (files.set(files.get()+','+self.funcs.select_file_pdf()),files_box.update()))
+                output_open_file = tk.Button(frame, text="Open File", command=lambda: (output_path.set(output_path.get()+','+self.funcs.select_output_file_pdf()), output_path_box.update()))
 
-                file1_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (file1.set(self.funcs.select_file()), file1_box.update()))
-                file2_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (file2.set(self.funcs.select_file()), file2_box.update()))
-                output_path_icon_file = tk.Button(frame, text="Open File", command=lambda: (output_path.set(self.funcs.select_file()), output_path_box.update()))
-
-                file1_label.grid(column=1, row=1)
-                file2_label.grid(column=1, row=2)
+                # Packing everything
+                files_label.grid(column=1, row=1)
                 output_path_label.grid(column=1, row=3)
 
-                file1_box.grid(column=2, row=1)
-                file2_box.grid(column=2, row=2)
+                files_box.grid(column=2, row=1)
                 output_path_box.grid(column=2, row=3)
 
-                file1_path_icon_file.grid(column=3, row=1, padx=3, pady=2)
-                file2_path_icon_file.grid(column=3, row=2, padx=3, pady=2)
-                output_path_icon_file.grid(column=3, row=3, padx=3, pady=2)
+                files_open_file.grid(column=3, row=1, padx=3, pady=2)
+                # file2_.grid(column=3, row=2, padx=3, pady=2)
+                output_open_file.grid(column=3, row=3, padx=3, pady=2)
 
                 submit_button.grid(column=1, row=4)
                 back_to_homepage.grid(column=3, row=5, pady=10)
 
+                title.pack()
+                instructions.pack(side='bottom', fill=tk.X)
                 frame.pack()
 
+            case 'convert_images_to_pdf':
+
+                # Creating required labels and frames
+                title = tk.Label(self, text='convert_images_to_pdf', font='Sans-Serif 20')
+                list_of_files_instructions = tk.Label(self, text='Have values in the entry box of list of files comma seperated', relief=tk.SUNKEN, anchor='s')
+                frame = tk.Frame(self)
+
+                # Creating variables for entryboxes
+                list_of_file = tk.StringVar()
+                list_of_file.set('')
+                output_path = tk.StringVar()
+                output_path.set('')
+
+                # Actual function which will do the task
+                def convert_images_to_pdf(list_of_images: list, output_pdf: str):
+                    list_of_files_instructions.pack_forget()
+                    frame.pack_forget()
+                    result = self.funcs.convert_images_to_pdf(list_of_images, output_pdf)
+                    lab = tk.Label(self, text=result)
+                    go_to_homepage = tk.Button(self, text='Back to homepage', command=lambda: (lab.pack_forget(), title.pack_forget(), self.window()))
+                    lab.pack()
+                    go_to_homepage.pack()
+
+                # Creating Labels for entry boxes
+                list_of_file_label = tk.Label(frame, text='List of files: ')
+                output_path_label = tk.Label(frame, text='Output filename: ')
+
+                # Creating entry boxes
+                list_of_file_entry = tk.Entry(frame, textvariable=list_of_file)
+                output_path_entry = tk.Entry(frame, textvariable=output_path)
+
+                # Creating required buttons
+                if list_of_file.get() == '':
+                    list_of_files_open_file = tk.Button(frame, text="Open File", command=lambda: (list_of_file.set(self.funcs.select_file_image()),list_of_file_entry.update()))
+                list_of_files_open_file = tk.Button(frame, text="Open File", command=lambda: (list_of_file.set(list_of_file.get()+','+self.funcs.select_file_image()),list_of_file_entry.update()))
+                output_path_open_file = tk.Button(frame, text="Open File", command=lambda: (output_path.set(self.funcs.select_output_file_pdf()), output_path_box.update()))
+                submit_button = tk.Button(frame, text='Submit', command=lambda: (convert_images_to_pdf(list_of_file.get().split(',') ,output_path.get())))
+                back_to_homepage = tk.Button(frame, text='Back to homepage', command=lambda: (frame.pack_forget(), self.window()))
+
+                # Packing everything
+                list_of_file_label.grid(column=1, row=1)
+                output_path_label.grid(column=1, row=2)
+
+                list_of_file_entry.grid(column=2, row=1)
+                output_path_entry.grid(column=2, row=2)
+
+                list_of_files_open_file.grid(column=3, row=1, padx=3, pady=3)
+                output_path_open_file.grid(column=3, row=2, padx=3)
+
+                submit_button.grid(column=1, row=3)
+                back_to_homepage.grid(column=3, row=5)
+
+                title.pack()
+
+                frame.pack()
+
+                list_of_files_instructions.pack(side='bottom', fill=tk.X)
+
+            case _:
+                title = tk.Label(self, text='Choose correct option.')
+                frame = tk.Frame(self)
+                
+                back_to_homepage = tk.Button(frame, text='Back to HomePage', command=lambda: (title.pack_forget(), frame.pack_forget(), self.window()))
+
+                back_to_homepage.pack()
+                frame.pack()
+                title.pack()
 
 
 if __name__=='__main__':
--- pdf.py
+++ pdf.py
@@ -1,12 +1,13 @@
 import pypdf
 import os
+from PIL import Image
 from tkinter import filedialog as fd
 
 class incorrectNoPagesException:
     "Enter correct no. of pages"
 
 class functions:
-    def select_file(self):
+    def select_file_pdf(self):
         filetypes = (
         ('PDF files', '*.pdf'),
         ('All files', '*.*')
@@ -17,8 +18,32 @@ class functions:
             filetypes=filetypes)
         
         return file
+    
+    def select_output_file_pdf(self):
+        filetypes = (
+        ('PDF files', '*.pdf,*.PDF'),
+        ('All files', '*.*')
+        )
+        file = fd.asksaveasfilename(
+            title='Path of the output pdf',
+            initialdir='.',
+            filetypes=filetypes)
+        
+        return file
+    
+    def select_file_image(self):
+        filetypes = (
+        ('Image files', '*.jpg,*.png,*.bmp,*.jpeg'),
+        ('All files', '*.*')
+        )
+        file = fd.askopenfilename(
+            title='Path of the pdf',
+            initialdir='.',
+            filetypes=filetypes)
+        
+        return file
 
-    def split_pdf(self, input_file, output_file, start_page, end_page):
+    def split_pdf(self, input_file: str, output_file: str, start_page: int , end_page: int):
         """
         Split pages of a PDF file and save the result to a new file.
 
@@ -43,21 +68,21 @@ class functions:
 
 
             with open(input_file, "rb") as input_file:
-                pdf_reader = pypdf.PdfReader(input_file)
-                pdf_writer = pypdf.PdfWriter()
+                reader = pypdf.PdfReader(input_file)
+                writer = pypdf.PdfWriter()
 
-                if len(pdf_reader.pages)<start_page or len(pdf_reader.pages)<end_page:
+                if len(reader.pages)<start_page or len(reader.pages)<end_page:
                     raise incorrectNoPagesException
                 for page_num in range(start_page - 1, end_page):
-                    pdf_writer.add_page(pdf_reader.pages[page_num])
+                    writer.add_page(reader.pages[page_num])
 
                 with open(output_file, "wb") as output_file:
-                    pdf_writer.write(output_file)
+                    writer.write(output_file)
             return "Success"
         except Exception as e:
             return e
 
-    def reduce_pdf_size(self, file, reduced_file_name):
+    def reduce_pdf_size(self, file: str, reduced_file_name: str):
         """"
         Reduce the size of the file
 
@@ -76,22 +101,22 @@ class functions:
                 raise FileNotFoundError
 
 
-            pdf_reader = pypdf.PdfReader(file)
-            pdf_writer = pypdf.PdfWriter()
+            reader = pypdf.PdfReader(file)
+            writer = pypdf.PdfWriter()
 
-            for page in pdf_reader.pages:
-                pdf_writer.add_page(page)
+            for page in reader.pages:
+                writer.add_page(page)
 
-            pdf_writer.add_metadata(pdf_reader.metadata)
+            writer.add_metadata(reader.metadata)
 
             with open(reduced_file_name, "wb") as f:
-                pdf_writer.write(f)
+                writer.write(f)
             
             return "Success"
         except Exception as e:
             return e
 
-    def merge_pdfs(self, list_of_files, output_file):
+    def merge_pdfs(self, list_of_files: list, output_file: str):
         """
         Append pdfs given in the argument list_of_files to provided output_file
 
@@ -100,17 +125,173 @@ class functions:
             output_file (str): The path of the output file 
         """
         try:
-            pdf_writer = pypdf.PdfWriter()
             if not isinstance(list_of_files, list):
                 raise TypeError
             
-            for pdf in list_of_files:
-                pdf_writer.append(pdf)
+            writer = pypdf.PdfWriter()
             
-            pdf_writer.write(output_file)
-            pdf_writer.close()
+            for pdf in list_of_files:
+                writer.append(pdf)
+            
+            writer.write(output_file)
+            writer.close()
 
             return "Success"
         except Exception as e:
             return e
-        
+
+    def convert_images_to_pdf(self, list_of_images: list, output_path: str):
+        """
+        Convert the given image to a pdf
+
+        Args:
+            list_of_images (list): List of the images to be converted to pdf
+            output_path (str): Path of output file, where pdf will be stored
+        """
+        try:
+            if not isinstance(list_of_images, list):
+                raise TypeError
+            if not isinstance(output_path, str):
+                raise TypeError
+
+            for image_path in list_of_images:
+                if not os.path.exists(image_path):
+                    raise FileNotFoundError
+
+            image_obj_list = []
+
+            for image in list_of_images:
+                image_pil = Image.open(image)
+                img = image_pil.convert('RGB')
+                image_obj_list.append(img)
+
+            img = image_obj_list[0]
+            image_obj_list.pop(0)
+
+            img.save(fr'{output_path}', format='pdf', save_all=True, append_images=image_obj_list)
+            return "Success"
+
+        except Exception as e:
+            return e
+
+    def stamp_pdf(self, stamp: str, input_file: str, output_file: str): # Not ready yet
+            """
+        Stamp the pdf given in the input_file
+
+        Args:
+            stamp (str): The path of the stamp image
+            input_file (str): The path of the file which needs to be stamped
+            output_file (str): The path of the file to which stamped file will be stored
+        """
+
+        # try:
+        #     if not isinstance(stamp, str):
+        #         raise TypeError
+        #     if not isinstance(input_file, str):
+        #         raise TypeError
+        #     if not isinstance(output_file, str):
+        #         raise TypeError
+            
+        #     if not os.path.exists(stamp):
+        #         raise FileNotFoundError
+        #     if not os.path.exists(input_file):
+        #         raise FileNotFoundError
+
+        #     reader = pypdf.PdfReader(input_file)
+        #     writer = pypdf.PdfWriter()
+
+        #     page_indices = list(range(0, len(reader.pages)))
+        #     print(page_indices)
+
+        #     for index in page_indices:
+        #         content_of_page = reader.pages[index]
+        #         mediabox = content_of_page.mediabox
+        #         content_of_page.merge_page(stamp)
+        #         content_of_page.mediabox = mediabox
+        #         writer.add_page(content_of_page)
+
+        #     with open(output_file, 'wb') as f:
+        #         writer.write(f)
+
+        # except Exception as e:
+        #     print(e)
+            if not isinstance(stamp, str):
+                raise TypeError
+            if not isinstance(input_file, str):
+                raise TypeError
+            if not isinstance(output_file, str):
+                raise TypeError
+            
+            if not os.path.exists(stamp):
+                raise FileNotFoundError
+            if not os.path.exists(input_file):
+                raise FileNotFoundError
+
+            reader = pypdf.PdfReader(input_file)
+            writer = pypdf.PdfWriter()
+
+            page_indices = list(range(0, len(reader.pages)))
+            print(page_indices)
+
+            for index in page_indices:
+                content_page = reader.pages[index]
+                mediabox = content_page.mediabox
+                # content_page.merge_page(stamp)
+                content_page.mediabox = mediabox
+                writer.add_page(content_page)
+            with open(output_file, 'wb') as f:
+                writer.write(f)
+
+    def watermark(self, watermark: str, input_file: str, output_file: str): # Not ready yet
+        """
+        Watermark the file in the argument input_file
+
+        Args: 
+            watermark (str): Path of the image file that is going to be used as watermark.
+            input_file (str): Path of the input file that is going to have the watermark.
+            output_file (str): Path of the pdf where the pdf will stored.  
+        """
+        try:
+            if not isinstance(watermark, str):
+                raise TypeError
+            if not isinstance(input_file, str):
+                raise TypeError
+            if not isinstance(output_file, str):
+                raise TypeError
+            
+            if not os.path.exists(watermark):
+                raise FileNotFoundError
+            if not os.path.exists(input_file):
+                raise FileNotFoundError
+            
+            self.convert_images_to_pdf([watermark], f'{watermark}.pdf')
+
+            real_watermark = watermark
+
+            reader = pypdf.PdfReader(input_file)
+            page_indices = list(range(0, len(reader.pages)))
+
+            writer = pypdf.PdfWriter()
+            
+            for index in page_indices:
+                content_page = reader.pages[index]
+                mediabox = content_page.mediabox
+
+                watermark_reader = pypdf.PdfReader(f'{real_watermark}.pdf')
+                watermark = watermark_reader.pages[0]
+
+                watermark.merge_page(content_page)
+                watermark.mediabox = mediabox
+                writer.add_page(watermark)
+                
+            os.remove(f'{real_watermark}.pdf')
+
+            with open(output_file, "wb") as f:
+                writer.write(f)
+            return "Success"
+
+        except Exception as e:
+            return e
+
+if __name__=='__main__':
+    funcs = functions()
